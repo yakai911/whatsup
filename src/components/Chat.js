bfd082a72a4d76e858d3237ddbd5ca1cdde27a63
bigbother <793041663@qq.com>
2020-09-27 00:33:23 +0800

    sidebar styled

---
+++ src/components/Chat.js
@@ -0,0 +1,39 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import React from "react";
+import "../assets/Chat.css";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p>
+          <span className="chat__name">Soney</span>
+          This is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
