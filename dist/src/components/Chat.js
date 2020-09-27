9ad77acf37ae92504e6c0dab104e69b4e8f08388
bigbother <793041663@qq.com>
2020-09-27 21:47:16 +0800

    UI finished

--- dist/src/components/Chat.js
+++ dist/src/components/Chat.js
@@ -1,5 +1,11 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
 import React from "react";
 import "../assets/Chat.css";
 
@@ -26,11 +32,28 @@ function Chat() {
       </div>
 
       <div className="chat__body">
-        <p>
+        <p className="chat__reciever chat__message">
+          <span className="chat__name">rddmg</span>
+          the plastic of universe
+          <span className="chat__timestamp ">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message">
           <span className="chat__name">Soney</span>
           This is message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            type="text"
+            // onChange={(e) => setInput(e.target.value)}
+            // value={input}
+            placeholder="输入文字"
+          />
+        </form>
+        <Mic />
       </div>
     </div>
   );
